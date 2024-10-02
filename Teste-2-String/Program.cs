1ad201b3a126f5a80771d8dc2332e919bbf2175a
GabrielKameoka <gabrielkameoka@gmail.com>
2024-10-02 12:46:28 -0300

    finalizando os testes

---
+++ Teste-2-String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace teste1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Digite qualquer palavra");
+        string confere = Console.ReadLine();
+        string ToLower = confere.ToLower ();
+        Console.WriteLine(ToLower);
+        int contador = 0;
+
+        foreach (char caractere in ToLower)
+        {
+            if (caractere == 'a')
+            {
+                contador++;
+            }
+        }
+        if (contador > 0)
+        {
+            System.Console.WriteLine($" A string tem {contador} a letra 'a'.");
+        }
+        else
+        {
+            System.Console.WriteLine("A string não tem nenhuma letra 'a'.");
+        }
+    }
+}
